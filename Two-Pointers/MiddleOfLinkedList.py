32d205f9008f3fc2a1f2230f356a62c7fdbb432e
Sreenath <79475176+sreenathmmenon@users.noreply.github.com>
2021-11-02 23:07:56 +0530

    Adding middle of linked list problem

---
+++ Two-Pointers/MiddleOfLinkedList.py
@@ -0,0 +1,28 @@
+### Given the head of a singly Linked List. Return the middle node of the linked list
+### If there are 2 middle nodes, return the 2nd middle node
+# head = [1,2,3,4,5]
+## Note: In Linked List, we usually use the head node(first node) to represent whole list
+
+
+### Answer
+## Use 2 pointers - fast and slow.
+## Fast moves with 2 nodes at a time and slow moves 1 node at a time.
+## So, when fast reaches the end, slow will be in the middle position
+
+## Time = O(n/2) = O(n) [No constants, they are cut off of n/2 = n]
+
+class ListNode:
+    def __init__(self, val, next = None):
+        self.val = val
+        self.next = next
+    
+def middleOfLinkedList(head: ListNode) -> ListNode:
+    # Initialize pointers to first node
+    slow = fast = head
+    while fast and fast.next:
+        # Fast pointer will move by 2 nodes
+        fast = fast.next.next
+        # Slow pointer will move by 1 node.
+        # Slow will be at middle element when fast reaches the end
+        slow = slow.next
+    return slow
